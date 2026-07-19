ddc44203743f26a366ff8f5b8a34270300bf1aa3
Keerthana-A-Mainframe <Keerthanaamar1999@gmail.com>
2026-07-19 13:10:27 +0530

    Fix cobolcheck command to use correct script name

--- .github/scripts/mainframe_operations.sh
+++ .github/scripts/mainframe_operations.sh
@@ -34,7 +34,7 @@ run_cobolcheck() {
   echo "Running cobolcheck for $program"
 
   # Run cobolcheck, but don't exit if it fails
-  ./cobolcheck -p $program
+  ./cobol-check -p $program
   echo "Cobolcheck execution completed for $program (exceptions may have occurred)"
 
   # Check if CC##99.CBL was created, regardless of cobolcheck exit status
